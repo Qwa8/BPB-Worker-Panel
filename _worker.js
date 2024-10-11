dab1fc96c771ca212943bfc4e2d19bbed8a06fc5
Qwa8 <163165385+Qwa8@users.noreply.github.com>
2024-10-11 23:28:11 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'cf03c954-84bc-4e07-b85a-37feb0c06ab5';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
@@ -287,7 +287,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        url.hostname = 'maimai.sega.jp';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
